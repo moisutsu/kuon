9ef195f5dce8f13497d95a805ad7b616950ca664
hppRC <hpp.ricecake@gmail.com>
2021-02-15 01:06:17 +0900

    :tada: Add warning

--- src/constants.rs
+++
@@ -1,7 +0,0 @@
-pub use percent_encoding::{AsciiSet, CONTROLS, NON_ALPHANUMERIC};
-
-pub(crate) const FRAGMENT: AsciiSet = NON_ALPHANUMERIC
-    .remove(b'*')
-    .remove(b'-')
-    .remove(b'.')
-    .remove(b'_');
--- src/models/bearer.rs
+++
@@ -1,7 +0,0 @@
-use serde_derive::*;
-
-#[derive(Deserialize, Debug, Clone, PartialEq)]
-pub struct BearerToken {
-    pub access_token: String,
-    pub token_type: String,
-}
